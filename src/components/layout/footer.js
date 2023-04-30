83348c3f883802f763b537f728b2907605fe4146
Senali <senalidilumika@gmail.com>
2023-05-01 00:24:38 +0530

    Added landing page

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -1,25 +1,35 @@
 import { company } from "../../utils/typo";
 
-const Footer = () => {
+const Footer = ({ fontColor }) => {
   return (
     <footer className="w-full p-4 font-medium">
       <div className="pl-[80px] mx-auto max-w-[1080px] lg:pl-0">
         <div className="sm:flex sm:items-center sm:justify-between">
           <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
             {/* TODO: Add the logo here. */}
-            <span className="text-md text-[#5b7a8a] hover:text-[#a1a1a1] sm:text-center dark:text-[#3d7f91]">
+            <span
+              className={`text-md ${
+                fontColor ? fontColor : "text-[#5b7a8a] hover:text-[#a1a1a1]"
+              } sm:text-center dark:text-[#3d7f91]`}
+            >
               <a href="">{company.name}</a>
             </span>
             <a
               href="https://github.com/senali_d/DevJam"
               target="_blank"
-              className="text-[#5b7a8a] hover:text-[#a1a1a1] dark:text-[#3d7f91]"
+              className={`${
+                fontColor ? fontColor : "text-[#5b7a8a] hover:text-[#a1a1a1]"
+              } dark:text-[#3d7f91]`}
               rel="noreferrer"
             >
               GitHub
             </a>
           </div>
-          <span className="text-md text-[#5b7a8a] hover:text-[#a1a1a1] sm:text-center dark:text-[#3d7f91]">
+          <span
+            className={`text-md ${
+              fontColor ? fontColor : "text-[#5b7a8a] hover:text-[#a1a1a1]"
+            } sm:text-center dark:text-[#3d7f91]`}
+          >
             © {new Date().getFullYear()}{" "}
             <a href="" className="hover:underline">
               {company.name}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,56 @@
-import { Badge } from '@chakra-ui/react'
+import Footer from "@/components/layout/footer";
+import Link from "next/link";
+import { company } from "@/utils/typo";
+
+function BackgroundVideo({ video }) {
+  return (
+    <>
+      <video
+        autoPlay
+        loop
+        muted
+        className="hidden md:block absolute z-[-1] w-auto min-w-full min-h-full bottom-0 max-w-full  overflow-hidden"
+      >
+        <source src={video} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="hidden bg-[#030413] opacity-20 md:block absolute z-[-1] w-auto min-w-full min-h-full bottom-0 max-w-full  overflow-hidden"></div>
+    </>
+  );
+}
 
 export default function Home() {
   return (
-    <main>
-      <Badge colorScheme='green'>Default</Badge>
-    </main>
-  )
+    <>
+      <main className="">
+        <BackgroundVideo video="/banner.mp4" />
+        <div className="px-4 lg:px-0 mx-auto max-w-[1080px] flex justify-center flex-col min-h-[calc(100vh-56px)]">
+          <div className="flex justify-center flex-row">
+            <div className="flex flex-col justify-between text-center h-[calc(100vh-56px)] py-32">
+              <h1 className="text-4xl sm:text-5xl font-extrabold text-[#E4E4ED] ">
+                {company.name}
+              </h1>
+              <div className="mb-5 text-4xl tracking-tight font-extrabold text-[#E4E4ED] sm:text-5xl md:text-7xl lg:px-32 space-y-5">
+                <h2 className="block xl:inline">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing
+                </h2>
+                <h3 className="block text-[#AEACC9] font-medium text-2xl lg:px-32">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing
+                </h3>
+              </div>
+              <div>
+                <Link
+                  href="/dashboard"
+                  className="px-10 py-3 border-0 border-transparent text-base font-medium rounded-full text-white bg-gradient-to-r from-[#00bdff] via-[#008dff] to-[#a13bf7] hover:drop-shadow-[0_3px_5px_#7d7d7d] md:py-4 md:text-lg"
+                >
+                  Use {company.name}
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <Footer fontColor="text-[#E4E4ED] hover:text-[#a1a1a1]" />
+    </>
+  );
 }
