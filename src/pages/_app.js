4584c44d8a1dfc13c5941c65fb1ef12e28c8842a
Senali <senalidilumika@gmail.com>
2023-05-03 01:14:26 +0530

    Fix merge conflicts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,22 +1,20 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { ThemeProvider } from "next-themes";
 import "@/styles/globals.css";
-import "@/styles/globals.css";
 import { WagmiConfig } from "wagmi";
 import { client } from "@/utils/wagmi";
 import { ConnectKitProvider } from "connectkit";
 
 export default function App({ Component, pageProps }) {
   return (
-    <WagmiConfig client={client}>
-      <ConnectKitProvider theme="retro">
-      
-        <div className="min-h-[calc(100vh-68px)] pt-16 px-2 sm:px-4">
-          <ChakraProvider>
-            <Component {...pageProps} />
-          </ChakraProvider>
-        </div>
-      </ConnectKitProvider>
-    </WagmiConfig>
+    <ThemeProvider attribute="class">
+      <WagmiConfig client={client}>
+        <ConnectKitProvider theme="retro">
+            <ChakraProvider>
+              <Component {...pageProps} />
+            </ChakraProvider>
+        </ConnectKitProvider>
+      </WagmiConfig>
+    </ThemeProvider>
   );
 }
