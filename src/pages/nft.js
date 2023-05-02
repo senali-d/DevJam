28a96d228fcd9e12a231755fedd74782d22ea0f7
Rahul Kulkarni <rk@Rahuls-MacBook-Pro.local>
2023-05-02 23:31:44 +0530

    added create NFT contract

--- src/pages/nft.js
+++ src/pages/nft.js
@@ -6,9 +6,94 @@ import Input from "@/components/form-elements/input";
 import Button from "@/components/form-elements/button";
 import Upload from "@/components/form-elements/upload";
 import Banner from "@/components/banner";
+import { Web3Storage } from "web3.storage";
+import { ABI } from "../contracts/launchpad.json"
+import { contracAddress } from "../utils/constant";
+import { useToast } from "@chakra-ui/react";
+import { useEffect } from "react";
+import {
+  useAccount,
+  useContractWrite,
+  usePrepareContractWrite,
+  useWaitForTransaction,
+} from "wagmi";
 
 const Token = () => {
   const [image, setImage] = useState('')
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
+  const [supply, setSupply] = useState('')
+  const [price, setPrice] = useState('')
+  const [maxSupplyFlag, setMaxSupplyFlag] = useState(true);
+  const { address } = useAccount();
+  const [uri, setUri] = useState('');
+  const toast = useToast();
+
+  const { config } = usePrepareContractWrite({
+    address: contracAddress,
+    abi: ABI,
+    functionName: "createNFT",
+    args: [
+      uri,
+      supply,
+      maxSupplyFlag == true ? 0 : 1,
+      parseInt(price),
+      address,
+    ],
+
+    onError: (error) => {
+      console.log("Error", error);
+    },
+    onSuccess: (result) => {
+      console.log(uri, supply, maxSupplyFlag == true ? 0 : 1, price, address);
+      console.log("Success", result);
+    },
+  });
+
+  const { data, write, error } = useContractWrite(config);
+
+  const { isSuccess } = useWaitForTransaction({ hash: data?.hash });
+
+  const createMetadata = () => {
+    var metadata = {
+      name: name,
+      description: description,
+      image: image,
+    };
+    console.log(metadata);
+    const client = new Web3Storage({
+      token:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDkxZTRjOEMwNTJiMzkzNEQ3Nzc5NWM3QWQ3MkQ0MTFhMGQyMWUxODIiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NzE2ODYwNTU1NjIsIm5hbWUiOiJNYXRpYy1Qcm9maWxlIn0.zDWjIoqZUCnPXtvWXjm_ZbvPN2ZZHTfcK7JHdM2S7hk",
+    });
+    client
+      .put([new File([JSON.stringify(metadata)], "metadata.json")])
+      .then(async (cid) => {
+        setUri(`https://${cid}.ipfs.w3s.link/metadata.json`);
+      });
+  };
+
+  useEffect(() => {
+    console.log("isSuccess", isSuccess);
+    console.log("error", error);
+    if (isSuccess) {
+      toast({
+        title: "NFT Created",
+        description: "Your NFT has been created successfully",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+    if (error) {
+      toast({
+        title: "Error",
+        description: "There was an error creating your token: " + error.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+  }, [isSuccess, error]);
   return (
     <Layout headTitle="Create Token">
       <div className="flex flex-col space-y-8 justify-center items-center max-w-[800px] mx-auto pb-32 pl-[60px] lg:pl-0">
@@ -23,6 +108,7 @@ const Token = () => {
             className="mx-auto"
             src={image !== "" ? image : "/token.png"}
             alt="preview"
+            loader={() => image}
             width={200}
             height={200}
           />
@@ -31,7 +117,20 @@ const Token = () => {
             name="image"
             label="Image"
             type="file"
-            onChange={(e) => {}}
+            onChange={(e) => {
+              const image = URL.createObjectURL(e.target.files[0]);
+              setImage(image);
+              const files = e.target.files;
+              const client = new Web3Storage({
+                token:
+                  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDkxZTRjOEMwNTJiMzkzNEQ3Nzc5NWM3QWQ3MkQ0MTFhMGQyMWUxODIiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NzE2ODYwNTU1NjIsIm5hbWUiOiJNYXRpYy1Qcm9maWxlIn0.zDWjIoqZUCnPXtvWXjm_ZbvPN2ZZHTfcK7JHdM2S7hk",
+              });
+              client.put(files).then((cid) => {
+                console.log(cid);
+                setImage(`https://${cid}.ipfs.w3s.link/${files[0].name}`);
+              });
+
+            }}
           />
           <Input
             id="name"
@@ -39,7 +138,9 @@ const Token = () => {
             label="Name"
             placeholder="Coinverse DAO"
             type="text"
-            onChange={(e) => {}}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
             helper="This Can Be Your DAO Name or Special Access Collection"
           />
           <Input
@@ -48,11 +149,15 @@ const Token = () => {
             label="Description"
             placeholder="Coinverse DAO Memberships"
             type="text"
-            onChange={(e) => {}}
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
             helper="Write Something About This NFT or Features"
           />
           <Checkbox
-            onChange={(e) => {}}
+            onChange={(e) => {
+              setMaxSupplyFlag(e.target.checked)
+            }}
             defaultChecked
             className="text-[#5b7a8a] dark:text-[#3d7f91]"
           >
@@ -64,7 +169,9 @@ const Token = () => {
             label="Max Supply"
             placeholder="0"
             type="number"
-            onChange={(e) => {}}
+            onChange={(e) => {
+              setSupply(e.target.value);
+            }}
             helper="Recommended Max Supply - 10 Million Tokens."
           />
           <Input
@@ -73,10 +180,30 @@ const Token = () => {
             label="Price"
             placeholder="1 5IRE"
             type="number"
-            onChange={(e) => {}}
+            onChange={(e) => {
+              setPrice(e.target.value);
+            }}
             helper="Recommend initial NFT Price - 2 5IRE, No '5IRE' Symbol Required."
           />
-          <Button label="Create NFT" onClick={() => {}} />
+          <button
+            onClick={async (e) => {
+              e.preventDefault();
+              createMetadata();
+            }}
+            className="w-full text-[#fffff] bg-violet-500 hover:bg-violet-600 focus:ring-1 focus:outline-none focus:ring-[#cfcfcf] font-medium rounded-lg text-sm px-5 py-2.5 text-center shadow-none dark:bg-violet-500 dark:hover:bg-violet-600 dark:text-gray-100"
+          >
+            Upload
+          </button>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+            write();
+            }}
+            className="w-full text-[#fffff] bg-violet-500 hover:bg-violet-600 focus:ring-1 focus:outline-none focus:ring-[#cfcfcf] font-medium rounded-lg text-sm px-5 py-2.5 text-center shadow-none dark:bg-violet-500 dark:hover:bg-violet-600 dark:text-gray-100"
+          >
+            {" "}
+            Create
+          </button>
         </form>
       </div>
     </Layout>
